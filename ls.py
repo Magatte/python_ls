e46e106607409a426bed873d8f8c4f886f8cea42
Magatte BA <magattestg@gmail.com>
2019-04-20 01:12:55 +0200

    (Add) Option -a

--- ls.py
+++ ls.py
@@ -6,8 +6,16 @@ import argparse
 def ls():
     parser = argparse.ArgumentParser(description='list files in a directory')
     parser.add_argument('directory', type=str, nargs='?', default='.')
+    parser.add_argument('--all', '-a', action='store_true', help='Include dotfiles in listing')
     args = parser.parse_args()
-    for elem in os.listdir(args.directory):
+
+    dirs = os.listdir(args.directory)
+
+    if args.all:
+        dirs += [os.curdir, os.pardir]
+    else:
+        dirs = [dir for dir in dirs if dir[0] != '.']
+    for elem in dirs:
         print elem
 
 if __name__ == '__main__':
