e0c2a14a5828b8a83e0a787fa0cccc5d0ceaed88
Magatte BA <magattestg@gmail.com>
2019-04-20 02:42:11 +0200

    (Add) Catching OS errors

--- ls.py
+++ ls.py
@@ -3,12 +3,14 @@
 import os
 import argparse
 
-def ls():
+def parse_args():
     parser = argparse.ArgumentParser(description='list files in a directory')
     parser.add_argument('directory', type=str, nargs='?', default='.')
     parser.add_argument('--all', '-a', action='store_true', help='Include dotfiles in listing')
-    args = parser.parse_args()
+    return parser.parse_args()
 
+def ls(args):
+    
     dirs = os.listdir(args.directory)
 
     if args.all:
@@ -17,9 +19,13 @@ def ls():
         dirs = [dir for dir in dirs if dir[0] != '.']
     
     dirs.sort()
-    
+
     for elem in dirs:
         print elem
 
 if __name__ == '__main__':
-    ls()
+    try:
+        args = parse_args();
+        ls(args)
+    except OSError as err:
+        print err
