a8991f38d55f0c3325a3b5d3290836a1fa40cfb9
magatte <magattestg@gmail.com>
2019-04-23 09:38:52 +0200

    (Add) 14 tests working

--- test/testLsRecursive.py
+++ test/testLsRecursive.py
@@ -25,7 +25,7 @@ class testFunctionLs(unittest.TestCase):
         capturedOutput = io.StringIO()                      # Create StringIO object
         sys.stdout = capturedOutput                         # and redirect stdout.
         self.options['all'] = False
-        self.options['recursive'] = False
+        self.options['recursive'] = True
         self.options['onlyDir'] = False
         self.options['long'] = False
         self.options['reverse'] = False
@@ -40,7 +40,7 @@ class testFunctionLs(unittest.TestCase):
     def testLsRecursiveDirectoryNotExist(self):
         self.path = 'test/toto'
         self.options['all'] = False
-        self.options['recursive'] = False
+        self.options['recursive'] = True
         self.options['onlyDir'] = False
         self.options['long'] = False
         self.options['reverse'] = False
@@ -48,86 +48,85 @@ class testFunctionLs(unittest.TestCase):
         with self.assertRaises(OSError):
             ls.lsRecursive(self.path, self.options)
 
-    # def testLsAll(self):
-    #     capturedOutput = io.StringIO()                      # Create StringIO object
-    #     sys.stdout = capturedOutput                         # and redirect stdout.
-    #     self.options['all'] = True
-    #     self.options['recursive'] = False
-    #     self.options['onlyDir'] = False
-    #     self.options['long'] = False
-    #     self.options['reverse'] = False
-    #     self.options['count'] = False
-    #     expectedOutput = ['.', '..', '.ignore', '.zombie', 'Dockerfile', 'Durex', 'Durex.service', 'Makefile', 'README.md', 'clean.sh', 'clients', 'daemon', 'includes', 'obj', 'reset.sh', 'show_log.sh', 'src']
-    #     with contextlib.redirect_stdout(capturedOutput):
-    #         ls.ls(self.path, self.options)
-    #     sys.stdout = sys.__stdout__                         # Reset redirect
-    #     self.assertEqual(len(expectedOutput), len(capturedOutput.getvalue().split()))
-    #     self.assertEqual(expectedOutput, capturedOutput.getvalue().split())
+    def testLsRecursiveAll(self):
+        capturedOutput = io.StringIO()                      # Create StringIO object
+        sys.stdout = capturedOutput                         # and redirect stdout.
+        self.options['all'] = True
+        self.options['recursive'] = True
+        self.options['onlyDir'] = False
+        self.options['long'] = False
+        self.options['reverse'] = False
+        self.options['count'] = False
+        expectedOutput = [bcolors.OKBLUE + '--', self.path + bcolors.ENDC, '.', '..', 'rel', bcolors.OKBLUE + '--', self.path + '/rel' + bcolors.ENDC, '.', '..', 'src', bcolors.OKBLUE + '--', self.path + '/rel/src' + bcolors.ENDC, '.', '..', 'install.d', 'install.o', 'main.d', 'main.o']
+        with contextlib.redirect_stdout(capturedOutput):
+            ls.lsRecursive(self.path, self.options)
+        sys.stdout = sys.__stdout__                         # Reset redirect
+        self.assertEqual(len(expectedOutput), len(capturedOutput.getvalue().split()))
+        self.assertEqual(expectedOutput, capturedOutput.getvalue().split())
 
-    # def testLsOnlyDir(self):
-    #     capturedOutput = io.StringIO()                      # Create StringIO object
-    #     sys.stdout = capturedOutput                         # and redirect stdout.
-    #     self.options['all'] = False
-    #     self.options['recursive'] = False
-    #     self.options['onlyDir'] = True
-    #     self.options['long'] = False
-    #     self.options['reverse'] = False
-    #     self.options['count'] = False
-    #     expectedOutput = ['.', '..', '.zombie', 'clients', 'daemon', 'includes', 'obj', 'src', '9', 'files']
-    #     with contextlib.redirect_stdout(capturedOutput):
-    #         ls.ls(self.path, self.options)
-    #     sys.stdout = sys.__stdout__                         # Reset redirect
-    #     self.assertEqual(len(expectedOutput), len(capturedOutput.getvalue().split()))
-    #     self.assertEqual(expectedOutput, capturedOutput.getvalue().split())
+    def testLsRecursiveOnlyDir(self):
+        capturedOutput = io.StringIO()                      # Create StringIO object
+        sys.stdout = capturedOutput                         # and redirect stdout.
+        self.options['all'] = False
+        self.options['recursive'] = True
+        self.options['onlyDir'] = True
+        self.options['long'] = False
+        self.options['reverse'] = False
+        self.options['count'] = False
+        expectedOutput = [bcolors.OKBLUE + '--', self.path + bcolors.ENDC, '.', '..', 'rel', '0', 'file', bcolors.OKBLUE + '--', self.path + '/rel' + bcolors.ENDC, '.', '..', 'src', '0', 'file', bcolors.OKBLUE + '--', self.path + '/rel/src' + bcolors.ENDC, '.', '..', '4', 'files']
+        with contextlib.redirect_stdout(capturedOutput):
+            ls.lsRecursive(self.path, self.options)
+        sys.stdout = sys.__stdout__                         # Reset redirect
+        self.assertEqual(len(expectedOutput), len(capturedOutput.getvalue().split()))
+        self.assertEqual(expectedOutput, capturedOutput.getvalue().split())
 
-    # def testLsLong(self):
-    #     capturedOutput = io.StringIO()                      # Create StringIO object
-    #     sys.stdout = capturedOutput                         # and redirect stdout.
-    #     self.options['all'] = False
-    #     self.options['recursive'] = False
-    #     self.options['onlyDir'] = False
-    #     self.options['long'] = True
-    #     self.options['reverse'] = False
-    #     self.options['count'] = False
-    #     expectedOutput = ['463', 'Dockerfile', '28592', 'Durex', '227', 'Durex.service', '3863', 'Makefile', '6856', 'README.md', '243', 'clean.sh', '4096', 'clients', '4096', 'daemon', '4096', 'includes', '4096', 'obj', '39', 'reset.sh', '84', 'show_log.sh', '4096', 'src']
-    #     with contextlib.redirect_stdout(capturedOutput):
-    #         ls.ls(self.path, self.options)
-    #     sys.stdout = sys.__stdout__                         # Reset redirect
-    #     self.assertEqual(len(expectedOutput), len(capturedOutput.getvalue().split()))
-    #     self.assertEqual(expectedOutput, capturedOutput.getvalue().split())
+    def testLsRecursiveLong(self):
+        capturedOutput = io.StringIO()                      # Create StringIO object
+        sys.stdout = capturedOutput                         # and redirect stdout.
+        self.options['all'] = False
+        self.options['recursive'] = True
+        self.options['onlyDir'] = False
+        self.options['long'] = True
+        self.options['reverse'] = False
+        self.options['count'] = False
+        expectedOutput = [bcolors.OKBLUE + '--', self.path + bcolors.ENDC, '4096', 'rel', bcolors.OKBLUE + '--', self.path + '/rel' + bcolors.ENDC, '4096', 'src', bcolors.OKBLUE + '--', self.path + '/rel/src' + bcolors.ENDC, '194', 'install.d', '1816', 'install.o', '188', 'main.d', '3040', 'main.o']
+        with contextlib.redirect_stdout(capturedOutput):
+            ls.lsRecursive(self.path, self.options)
+        sys.stdout = sys.__stdout__                         # Reset redirect
+        self.assertEqual(len(expectedOutput), len(capturedOutput.getvalue().split()))
+        self.assertEqual(expectedOutput, capturedOutput.getvalue().split())
 
-    # def testLsReverse(self):
-    #     capturedOutput = io.StringIO()                      # Create StringIO object
-    #     sys.stdout = capturedOutput                         # and redirect stdout.
-    #     self.options['all'] = False
-    #     self.options['recursive'] = False
-    #     self.options['onlyDir'] = False
-    #     self.options['long'] = False
-    #     self.options['reverse'] = True
-    #     self.options['count'] = False
-    #     expectedOutput = ['Dockerfile', 'Durex', 'Durex.service', 'Makefile', 'README.md', 'clean.sh', 'clients', 'daemon', 'includes', 'obj', 'reset.sh', 'show_log.sh', 'src']
-    #     expectedOutput.reverse()
-    #     with contextlib.redirect_stdout(capturedOutput):
-    #         ls.ls(self.path, self.options)
-    #     sys.stdout = sys.__stdout__                         # Reset redirect
-    #     self.assertEqual(len(expectedOutput), len(capturedOutput.getvalue().split()))
-    #     self.assertEqual(expectedOutput, capturedOutput.getvalue().split())
+    def testLsRecursiveReverse(self):
+        capturedOutput = io.StringIO()                      # Create StringIO object
+        sys.stdout = capturedOutput                         # and redirect stdout.
+        self.options['all'] = False
+        self.options['recursive'] = True
+        self.options['onlyDir'] = False
+        self.options['long'] = False
+        self.options['reverse'] = True
+        self.options['count'] = False
+        expectedOutput = [bcolors.OKBLUE + '--', self.path + bcolors.ENDC, 'rel', bcolors.OKBLUE + '--', self.path + '/rel' + bcolors.ENDC, 'src', bcolors.OKBLUE + '--', self.path + '/rel/src' + bcolors.ENDC, 'main.o', 'main.d', 'install.o', 'install.d']
+        with contextlib.redirect_stdout(capturedOutput):
+            ls.lsRecursive(self.path, self.options)
+        sys.stdout = sys.__stdout__                         # Reset redirect
+        self.assertEqual(len(expectedOutput), len(capturedOutput.getvalue().split()))
+        self.assertEqual(expectedOutput, capturedOutput.getvalue().split())
 
-    # def testLsAllOnlyDir(self):
-    #     capturedOutput = io.StringIO()                      # Create StringIO object
-    #     sys.stdout = capturedOutput                         # and redirect stdout.
-    #     self.options['all'] = True
-    #     self.options['recursive'] = False
-    #     self.options['onlyDir'] = True
-    #     self.options['long'] = False
-    #     self.options['reverse'] = False
-    #     self.options['count'] = False
-    #     expectedOutput = ['.', '..', '.zombie', 'clients', 'daemon', 'includes', 'obj', 'src', '9', 'files']
-    #     with contextlib.redirect_stdout(capturedOutput):
-    #         ls.ls(self.path, self.options)
-    #     sys.stdout = sys.__stdout__                         # Reset redirect
-    #     self.assertEqual(len(expectedOutput), len(capturedOutput.getvalue().split()))
-    #     self.assertEqual(expectedOutput, capturedOutput.getvalue().split())
+    def testLsAllOnlyDir(self):
+        capturedOutput = io.StringIO()                      # Create StringIO object
+        sys.stdout = capturedOutput                         # and redirect stdout.
+        self.options['all'] = True
+        self.options['recursive'] = False
+        self.options['onlyDir'] = True
+        self.options['long'] = False
+        self.options['reverse'] = False
+        self.options['count'] = False
+        expectedOutput = [bcolors.OKBLUE + '--', self.path + bcolors.ENDC, '.', '..', 'rel', '0', 'file', bcolors.OKBLUE + '--', self.path + '/rel' + bcolors.ENDC, '.', '..', 'src', '0', 'file', bcolors.OKBLUE + '--', self.path + '/rel/src' + bcolors.ENDC, '.', '..', '4', 'files']
+        with contextlib.redirect_stdout(capturedOutput):
+            ls.lsRecursive(self.path, self.options)
+        sys.stdout = sys.__stdout__                         # Reset redirect
+        self.assertEqual(len(expectedOutput), len(capturedOutput.getvalue().split()))
+        self.assertEqual(expectedOutput, capturedOutput.getvalue().split())
 
 
 if __name__ == '__main__':
